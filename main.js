dd2034965b277ac455239650d62bd51912304813
Ben Mangelsen <bmangelsen@gmail.com>
2016-12-05 13:24:07 -0500

    Answered first five problems

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
-// Solve your homework here.
-console.log("Hello World!");
+// Assign "Hello World" to a variable message
+var message = "Hello World";
+console.log(message);
+
+// Assign a different string to a different variable
+var question = "Hello World?";
+console.log(question);
+
+// Assign a number to a variable
+var age = 26;
+console.log(age);
+
+// Use string concatenation to display the number in a string.
+var sentence = "Her age is " + age;
+console.log(sentence);
+
+// Make an array of your favorite movies or books or bands. Have at least 4 values.
+var movies = ["The Fellowship of the Ring", "The Two Towers", "The Return of the King", "The Hobbit"];
+console.log(movies);

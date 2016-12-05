a6f39b368d02b24fabd1ab2cc66ba1afad896dda
Ben Mangelsen <bmangelsen@gmail.com>
2016-12-05 13:53:37 -0500

    Turned sentence into array and displayed words that are six characters long

--- main.js
+++ main.js
@@ -11,9 +11,22 @@ var age = 26;
 console.log(age);
 
 // Use string concatenation to display the number in a string.
-var sentence = "Her age is " + age;
-console.log(sentence);
+var myAge = "My age is " + age;
+console.log(myAge);
 
 // Make an array of your favorite movies or books or bands. Have at least 4 values.
 var movies = ["The Fellowship of the Ring", "The Two Towers", "The Return of the King", "The Hobbit"];
 console.log(movies);
+
+//Turn a sentence into an Array, and select the words that are six characters long.
+var sentence = "Schlitz four dollar toast godard truffaut bespoke neutra artisan pop-up. Chia keytar single-origin coffee sustainable disrupt.";
+var newSentence = sentence.replace(/\./g, "").split(" ");
+console.log(newSentence);
+
+function lengthCheck (word) {
+  if ( word.length == 6 ) {
+    console.log(word);
+  }
+}
+
+newSentence.forEach(lengthCheck);
